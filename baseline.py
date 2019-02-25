81cfab1ca381238f721b6c3d1cacf7c238bcb809
Hektor Misplon <36757790+hektormisplon@users.noreply.github.com>
2019-02-25 15:04:34 +0100

    Update baseline.py

--- baseline.py
+++ baseline.py
@@ -10,7 +10,7 @@ s = SenseHat()
 # string to show
 message = "Hello! We are New Media Development"
 
-# get random color - z index equals a background color - defaults to foreground
+# get random color - z index for background/foreground - defaults to foreground
 def get_rand_color(z = 1):
     r, g, b = randint(0,255), randint(0,255), randint(0,255)
     if z == 1: 

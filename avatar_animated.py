b216ddd6a1314cf0e5029263523cf101434ccdac
Hektor Misplon <hektor.misplon@gmail.com>
2019-02-25 23:00:53 +0100

    5 - wip

---
+++ avatar_animated.py
@@ -0,0 +1,10 @@
+from sense_hat import SenseHat
+import time
+from random import randint
+s = SenseHat()
+
+def generate_rand_avatar():
+
+def display_rand_avatar():
+
+# extra: color, char & speed settings from cli

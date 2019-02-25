69a04e7848d26de90ab3901a8c9cd0996fa856a7
Hektor Misplon <hektor.misplon@gmail.com>
2019-02-25 15:01:58 +0100

    2 - finished

---
+++ baseline.py
@@ -0,0 +1,25 @@
+from sense_hat import SenseHat
+import time
+from random import randint
+s = SenseHat()
+
+# show string w/ bg color (method show_message)
+# make color and bg color random, bg color should be darker
+# loop
+
+# string to show
+message = "Hello! We are New Media Development"
+
+# get random color - z index equals a background color - defaults to foreground
+def get_rand_color(z = 1):
+    r, g, b = randint(0,255), randint(0,255), randint(0,255)
+    if z == 1: 
+      return (r, g, b)
+    else:
+      return (r/10, g/10, b/10)
+
+# display the message
+def loop_message():
+    s.show_message(message, text_colour=get_rand_color(1), back_colour=get_rand_color(0))
+    loop_message()
+loop_message()

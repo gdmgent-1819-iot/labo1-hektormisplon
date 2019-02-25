335c71bc7b26332781951411e3efdb82bd25fc88
Hektor Misplon <hektor.misplon@gmail.com>
2019-02-25 22:32:19 +0100

    4 - wip

--- pixelator.py
+++ pixelator.py
@@ -6,15 +6,6 @@ s = SenseHat()
 # loop through all leds w/ random color
 # apply easing funciton
 # extra: add easing via cli
-
-from sense_hat import SenseHat
-import time
-from random import randint
-s = SenseHat()
-
-# loop through all leds w/ random color
-# apply easing funciton
-# extra: add easing via cli
 s.clear()
 
 def ease_in_out(amount):
@@ -23,7 +14,7 @@ def ease_in_out(amount):
 def loop_matrix():
   for i in range(8):
       for j in range(8):
-          s.set_pixel(i,j, (255,255,255))
+          s.set_pixel(i,j, (randint(255), randint(255), randint(255)))
           time.sleep(ease_in_out(.5))
   s.clear()
   loop_matrix()
